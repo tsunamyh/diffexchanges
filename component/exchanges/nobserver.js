56f79d4b9d7482519d6e7e3941a7639072c17bc5
sina fathpour <sina.fathpoor@gmail.com>
2023-03-30 02:28:11 +0330

    return[feeR,hajm,feettri]

---
+++ component/exchanges/nobserver.js
@@ -0,0 +1,41 @@
+const {nobCoin} = require("../../symbols/symbols");
+
+const axios = require("axios").default;
+
+const nobBaseUrl = "https://api.nobitex.ir/v2/";
+
+const nobInstance = axios.create({
+  baseURL: nobBaseUrl,
+});
+
+async function httpGetNobOrderBooks(symbol) {
+  // nobInstance.defaults.params = { symbol }
+  const response = await nobInstance.get("/orderbook/" + symbol)
+
+  const orderBooks = sortOrderBooks(response.data)
+  console.log("nobOrderBooks:>", orderBooks);
+  return orderBooks
+}
+
+function sortOrderBooks(data) {
+  const ttrAsk = data["USDTIRT"].bids[0][0]
+  const ttrBid = data["USDTIRT"].asks[0][0]
+  const orderBooks = {}
+  nobCoin.forEach(function (symbol) {
+    const ask = data[symbol[0]].bids[0];
+    const bid = data[symbol[0]].asks[0]
+    if (ask && bid) {
+      // [feeRiali,hajm,feettri]
+      orderBooks[symbol[0]] = {
+        ask : [...ask,(ask[0]/ttrBid)],
+        bid : [...bid,(bid[0]/ttrAsk)]
+      }
+    }
+  })
+
+  return orderBooks
+}
+
+module.exports = {
+  httpGetNobOrderBooks
+}
