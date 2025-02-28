caa77b41ee1032cfae5638ff49d0275978de9696
Hojat Fathpour <fathpour.hojat@gmail.com>
2025-02-28 20:38:20 +0330

    commit

--- component/exchanges/coinserver.js
+++ component/exchanges/coinserver.js
@@ -2,9 +2,9 @@ const axios = require("axios").default;
 const HttpsAgent = require("https-proxy-agent");
 const { coin } = require("../../symbols/symbols");
 
-const agent = new HttpsAgent("http://192.168.1.11:8081");
+const agent = new HttpsAgent("http://127.0.0.1:10809");
 
-const coinBaseUrl = "https://api.coinex.com/v1";
+const coinBaseUrl = "https://api.coinex.com/v2";
 
 const coinInstance = axios.create({
   baseURL: coinBaseUrl,
@@ -14,15 +14,15 @@ const coinInstance = axios.create({
 // httpGetCoinOrderBooks()
 async function httpGetCoinOrderBooks() {
 
-  const response = await coinInstance.get("/market/ticker/all");
-  let coinOrderBook = sortOrderBooks(response.data.data.ticker);
+  const response = await coinInstance.get("/spot/ticker");
+  let coinOrderBook = sortOrderBooks(response.data.data/* .ticker */);
   // console.log(coinOrderBook);
   return coinOrderBook;
 }
 
 function sortOrderBooks(data) {
   const orderBooks = {};
-  coin.forEach(function (symbol) {
+  coin.forEach(function (symbol,index) {
     if (data.hasOwnProperty(symbol)) {
       const ask = data[symbol].sell
       const bid = data[symbol].buy
--- component/exchanges/nobserver.js
+++ component/exchanges/nobserver.js
@@ -11,7 +11,8 @@ const nobInstance = axios.create({
 async function httpGetNobOrderBooks(symbol) {
   // nobInstance.defaults.params = { symbol }
   const response = await nobInstance.get("/orderbook/" + symbol)
-
+  // console.log("re",response.data);
+  
   const orderBooks = sortOrderBooks(response.data)
   // console.log("nobOrderBooks:>", orderBooks);
   return orderBooks
@@ -22,24 +23,41 @@ function sortOrderBooks(data) {
   const ttrBid = data["USDTIRT"].asks[0][0]
   const orderBooks = {}
   nobCoin.forEach(function (symbol) {
-    const ask = data[symbol[0]].bids[0];
-    const bid = data[symbol[0]].asks[0]
-    if (ask && bid) {
-      // [feeRiali,hajm,feettri]
-      if (symbol[0] == "SHIBIRT") {
-        ask[0] = ask[0]/1000
-        bid[0] = bid[0]/1000
-      }
-      orderBooks[symbol[0]] = {
-        ask : [(ask[0]/ttrBid),...ask],
-        bid : [(bid[0]/ttrAsk),...bid]
+
+    if (!(data[symbol[0]]?.bids === undefined || data[symbol[0]]?.bids.length == 0)) {
+      // console.log("data[symbol[0]].bids[0]",symbol[0],data[symbol[0]]?.bids[0]);
+      // array exist or is not empty
+      const ask = data[symbol[0]]?.bids[0];
+      const bid = data[symbol[0]]?.asks[0]
+      if (ask && bid) {
+        // [feeRiali,hajm,feettri]
+        if (symbol[0] == "SHIBIRT") {
+          ask[0] = ask[0]/1000
+          bid[0] = bid[0]/1000
+        }
+        orderBooks[symbol[0]] = {
+          ask : [(ask[0]/ttrBid),...ask],
+          bid : [(bid[0]/ttrAsk),...bid]
+        }
       }
     }
   })
 
   return orderBooks
 }
+// httpGetNobOrderBooks("all")  //test
+getAllOrderBooks();
+// console.log("nobOrderBooks:",nobOrderBooks);
+async function getAllOrderBooks() {
 
+  const nobOrderBooksPromise = httpGetNobOrderBooks("all");
+
+  const promisesArray = [nobOrderBooksPromise/*, ...ramzOrderBooksPromise */];
+  // console.log(promisesArray);
+  const allOrderBooks = await Promise.allSettled(promisesArray);
+  console.log(allOrderBooks);
+  return allOrderBooks;
+}
 module.exports = {
   httpGetNobOrderBooks
 }
